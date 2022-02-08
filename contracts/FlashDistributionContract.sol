2fa7b2808202f6bb29f9b43f25afe8ff29ab0d60
Umar Salim <me@umarsalim.com>
2022-02-08 14:54:58 +0530

    feat: initial empty commit for distribution contract

---
+++ contracts/FlashDistributionContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.4;
+
+// === Distribution Contract ===
+
+// 1. Distributes FLASH tokens held inside contract
+// 2. Merkle root hash (data) can only be uploaded once and never changed
+// 3. Expiry date for all claims will be the same date and immutable
+// 5. After expiry, anyone can burn remainder of tokens held in contract
+
+contract FlashDistributionContract {
+
+}
